ebe266e3e5ed88707d11451d60812a84726c12ca
unknown <charanbasetti@gmail.com>
2021-03-25 10:03:50 -0500

    azure Main

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Welcome Charan React</code>
+          Edit <code>Welcome Charan React_main</code>
         </p>
         <a
           className="App-link"
